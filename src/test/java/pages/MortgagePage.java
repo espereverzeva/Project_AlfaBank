49fe1e1f6987c0e461e9e76ab853da111af6de92
Your Name <you@example.com>
2026-02-16 19:28:24 +0700

    Tests1

---
+++ src/test/java/pages/MortgagePage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MortgagePage {
+    private SelenideElement forPrivateIndividuals = $(byAttribute("data-test-id", "test-retail")),
+                            clickmortgageTab= $(byAttribute("data-test-id", "link-mortgage"));
+
+
+
+
+    public MortgagePage openPage() {
+        open("/");
+        return this;
+    }
+
+    public MortgagePage hoverOverTheIndividualsTab(String value) {
+        forPrivateIndividuals.$(byText(value)).shouldBe(visible).hover();
+        return this;
+    }
+
+    public MortgagePage mortgageTab() {
+        clickmortgageTab.shouldBe(visible).click();
+        return this;
+    }
+
+    public MortgagePage readyProperty(String value) {
+        $(byText(value)).click();
+        return this;
+    }
+
+    public MortgagePage checkMortgageRecord(String value) {
+        $(withText(value)).shouldBe(visible);
+        return this;
+    }
+}
--- src/test/java/tests/BankTests.java
+++ src/test/java/tests/BankTests.java
@@ -1,36 +1,63 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
+import pages.MortgagePage;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byAttribute;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
+
 
 public class BankTests extends TestBase {
 
+    MortgagePage mortragePage = new MortgagePage();
+
     @Test
- void mortgageForSecondaryHousing() {
-     open ("/");
-        //actions().moveToElement($(".k5qQXQ")).perform();
-        //$(byText("Частным лицам")).shouldBe(visible).hover();
-        //aXYDeT gXYDeT cXYDeT
-
-        $(byAttribute("data-test-id", "test-retail")).$(byText("Частным лицам")).shouldBe(visible).hover();
-        $(byAttribute("data-test-id", "link-mortgage")).shouldBe(visible).click();
-        $(byText("Готовая недвижимость")).click();
-        // $(".'aSfUAb vSfUAb'").shouldHave(text("Ипотека\n" + "на вторичное жильё"));
-
+    void mortgageForSecondaryHousing() {
+        mortragePage.openPage()
+        //open("/");
+        //$(byAttribute("data-test-id", "test-retail")).$(byText("Частным лицам")).shouldBe(visible).hover();
+                .hoverOverTheIndividualsTab("Частным лицам")
+        //$(byAttribute("data-test-id", "link-mortgage")).shouldBe(visible).click();
+                .mortgageTab()
+        //$(byText("Готовая недвижимость")).click();
+                .readyProperty("Готовая недвижимость")
+        //$(withText("Ипотека на вторичное жильё")).shouldBe(visible);
+                .checkMortgageRecord("Ипотека на вторичное жильё");
     }
 
     @Test
-    void tariffs() {
-        open ("/");
+    void businessProposal() {
+        open("/");
         $(byAttribute("data-test-id", "test-sme")).$(byText("Малому бизнесу и ИП")).shouldBe(visible).click();
-        $(".hgfDvQ").scrollTo().click();
-        $(".zyJgKm").scrollTo().click();
+        $(byText("условия для среднего и крупного бизнеса")).scrollTo().click();
+        $(byText("Все предложения для среднего и крупного бизнеса")).scrollTo().click();
+        $(withText("Внешнеэкономическая деятельность")).shouldBe(visible);
+    }
+
+    @Test
+    void infoAverageLargeBusiness() {
+        open("/");
+        $(byAttribute("data-test-id", "test-corporate")).$(byText("Среднему и крупному бизнесу")).shouldBe(visible).click();
+        $(byText("Подберём решения для вашего бизнеса")).scrollTo().click();
+        $(byAttribute("data-test-id", "accordion-header-1")).scrollTo().click();
+        $(withText("Как открыть расчётный счёт?")).shouldBe(visible);
+    }
+
+    @Test
+    void searchPage() {
+        open("/");
+        $(".v5qQXQ").click();
+        $(byAttribute("data-test-id", "search-input")).val("кредит").pressEnter();
+        $(".fdJZg3").shouldHave(text("кредит"));
+    }
+
+    @Test
+    void feedbackPage() {
+        open("/");
+        $(byText("Отзывы")).scrollTo().click();
+        $(withText("Отзывы об Альфа-Банке")).shouldBe(visible);
 
     }
 }
