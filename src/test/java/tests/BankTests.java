e5563d9a8d08e8107949a51f7953ea7f82bc07d8
Your Name <you@example.com>
2026-02-17 13:14:25 +0700

    delete comments

--- src/test/java/tests/BankTests.java
+++ src/test/java/tests/BankTests.java
@@ -14,58 +14,39 @@ public class BankTests extends TestBase {
     @Test
     void mortgageForSecondaryHousing() {
         mortragePage.openPage()
-                //open("/");
-                //$(byAttribute("data-test-id", "test-retail")).$(byText("Частным лицам")).shouldBe(visible).hover();
                 .hoverOverTheIndividualsTab("Частным лицам")
-                //$(byAttribute("data-test-id", "link-mortgage")).shouldBe(visible).click();
                 .mortgageTab()
-                //$(byText("Готовая недвижимость")).click();
                 .readyProperty("Готовая недвижимость")
-                //$(withText("Ипотека на вторичное жильё")).shouldBe(visible);
                 .checkMortgageRecord("Ипотека на вторичное жильё");
     }
 
     @Test
     void businessProposal() {
-        //open("/");
         mortragePage.openPage();
-        //$(byAttribute("data-test-id", "test-sme")).$(byText("Малому бизнесу и ИП")).shouldBe(visible).click();
         businessPage.smallBusinessIndividualEntrepreneurs("Малому бизнесу и ИП")
-                //$(byText("условия для среднего и крупного бизнеса")).scrollTo().click();
                 .conditionsLargeMediumBusiness("условия для среднего и крупного бизнеса")
-                //$(byText("Все предложения для среднего и крупного бизнеса")).scrollTo().click();
                 .allOffersLargeMediumBusiness("Все предложения для среднего и крупного бизнеса");
     }
 
     @Test
     void usefulArticles() {
-        //open("/");
         mortragePage.openPage();
-        //$(byText("Блог")).scrollTo().click();
         blogPage.scrollBlogTab("Блог")
-                //$(withText("Полезные статьи от Альфа-Банка")).shouldBe(visible);
                 .visibleBlogTab("Полезные статьи от Альфа-Банка");
     }
 
     @Test
     void searchInfoCredit() {
-        //open("/");
         mortragePage.openPage();
-        //$(".v5qQXQ").click();
         creditPage.clickSearch()
-                //$(byAttribute("data-test-id", "search-input")).val("кредит").pressEnter();
                 .setSearch("кредит")
-                //$(".fdJZg3").shouldHave(text("кредит"));
                 .checktSearch("кредит");
     }
 
     @Test
     void feedbackBank() {
-        //open("/");
         mortragePage.openPage();
-        //$(byText("Отзывы")).scrollTo().click();
         feedbackPage.scrollFeedback("Отзывы")
-                //$(withText("Отзывы об Альфа-Банке")).shouldBe(visible);
                 .checktFeedback("Отзывы об Альфа-Банке");
     }
 }
